56b8b1db22ca880d4a60f1597fdbb40269dfac27
rasdani <daniel.auras94@gmx.de>
2022-11-07 14:55:23 +0100

    modified ls alias

--- aliases.zsh
+++ aliases.zsh
@@ -4,6 +4,7 @@ alias gr='cd $(git rev-parse --show-toplevel)'
 alias ipy="ipython"
 alias tm="tmux"
 alias mysync="rsync -ah --partial --info=progress2"
+alias l="ls -a"
 
 function cl() {
     cd "$@" && l
