aa9a13a539c53c44a3179de36c9d5ea6da1b3840
rasdani <daniel.auras94@gmx.de>
2022-11-07 16:26:28 +0100

    added .zshrc

--- aliases.zsh
+++ aliases.zsh
@@ -5,6 +5,8 @@ alias ipy="ipython"
 alias tm="tmux"
 alias mysync="rsync -ah --partial --info=progress2"
 alias l="ls -a"
+alias j="fasd_cd -d"
+alias jj="fasd_cd -d -i"
 
 function cl() {
     cd "$@" && l
