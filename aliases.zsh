e58461eab7ae3a344fc49fc4ae214937cb367914
rasdani <daniel.auras94@gmx.de>
2022-11-20 16:25:57 +0100

    added du -h alias

--- aliases.zsh
+++ aliases.zsh
@@ -4,6 +4,7 @@ alias gr='cd $(git rev-parse --show-toplevel)'
 alias ipy="ipython"
 alias tm="tmux"
 alias mysync="rsync -ah --partial --info=progress2"
+alias duh= "du -h --max-depth=1"
 alias l="ls -a"
 alias j="fasd_cd -d"
 alias jj="fasd_cd -d -i"
